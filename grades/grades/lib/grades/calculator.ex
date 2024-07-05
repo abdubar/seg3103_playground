c117cbdf4b64af601341078e5caf70cfac312170
abdubar <144919862+abdubar@users.noreply.github.com>
2024-07-04 20:43:22 -0400

    q2.4 (optional) Refactor 3

--- grades/grades/lib/grades/calculator.ex
+++ grades/grades/lib/grades/calculator.ex
@@ -17,7 +17,7 @@ defmodule Grades.Calculator do
   end
 
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    aavg_homework = avg(homework)
+    avg_homework = avg(homework)
     avg_labs = avg(labs)
     mark = calculate_grade(avg_labs, avg_homework, midterm, final)
     round(mark * 100)
@@ -51,31 +51,15 @@ defmodule Grades.Calculator do
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
-
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
     avg_exams = (midterm + final) / 2
+    num_labs = Enum.reject(labs, fn mark -> mark < 0.25 end) |> Enum.count()
 
-    num_labs =
-      labs
-      |> Enum.reject(fn mark -> mark < 0.25 end)
-      |> Enum.count()
-
-    if avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3 do
+    if failed_to_participate?(avg_homework, avg_exams, num_labs) do
       0
     else
-      mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+      mark = calculate_grade(avg_labs, avg_homework, midterm, final)
 
       cond do
         mark > 0.895 -> 10
@@ -88,7 +72,7 @@ defmodule Grades.Calculator do
         mark > 0.545 -> 3
         mark > 0.495 -> 2
         mark > 0.395 -> 1
-        :else -> 0
+        true -> 0
       end
     end
   end
