a8116855976977de63c61bfe84f9ee4d7580f197
abdubar <144919862+abdubar@users.noreply.github.com>
2024-07-04 22:40:28 -0400

    q1.3 Test cases

--- grades/grades/test/grades/calculator_test.exs
+++ grades/grades/test/grades/calculator_test.exs
@@ -3,14 +3,38 @@ defmodule Grades.CalculatorTest do
   alias Grades.Calculator
 
   describe "percentage_grade/1" do
-    test "sample" do
-      assert 85 ==
-               Calculator.percentage_grade(%{
-                 homework: [0.8],
-                 labs: [1, 1, 1],
-                 midterm: 0.70,
-                 final: 0.9
-               })
+    test "calculates percentage grade with empty homework and labs" do
+      input = %{homework: [], labs: [], midterm: 0.4, final: 0.4}
+      assert Calculator.percentage_grade(input) == 20
+    end
+
+    test "calculates percentage grade with minimal homework and labs" do
+      input = %{homework: [0.5], labs: [0.25, 0.25, 0.25], midterm: 0.4, final: 0.4}
+      assert Calculator.percentage_grade(input) == 40
+    end
+  end
+
+  describe "letter_grade/1" do
+    test "calculates letter grade with empty homework and labs" do
+      input = %{homework: [], labs: [], midterm: 0.4, final: 0.4}
+      assert Calculator.letter_grade(input) == "EIN"
+    end
+
+    test "calculates letter grade with minimal homework and labs" do
+      input = %{homework: [0.3, 0.3, 0.3], labs: [0.3, 0.3, 0.3], midterm: 0.8, final: 0.8}
+      assert Calculator.letter_grade(input) == "C"
+    end
+  end
+
+  describe "numeric_grade/1" do
+    test "calculates numeric grade with empty homework and labs" do
+      input = %{homework: [], labs: [], midterm: 0.4, final: 0.4}
+      assert Calculator.numeric_grade(input) == 0
+    end
+
+    test "calculates numeric grade with minimal homework and labs" do
+      input = %{homework: [0.3, 0.3, 0.3], labs: [0.3, 0.3, 0.3], midterm: 0.8, final: 0.8}
+      assert Calculator.numeric_grade(input) == 4
     end
   end
 end
